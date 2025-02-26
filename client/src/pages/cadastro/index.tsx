bac0cd149e61dca3b7183c476b040390047cccc2
Larissa Paloze <lpaloze@gmail.com>
2025-02-26 10:48:42 -0300

    Adicionei as minhas páginas, as rotas para as minhas páginas no App.tsx e o link para registre-se na página Login

---
+++ client/src/pages/cadastro/index.tsx
@@ -0,0 +1,9 @@
+const Cadastro = () => {
+    return (
+        <div>
+            <h1>Cadastro</h1>
+            <p>Preencha suas informações para se cadastrar.</p>
+        </div>
+    );
+};
+export default Cadastro;
---
+++ client/src/pages/contact/index.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Fale Conosco</h1>
+            <p>Entre em contato conosco através do formulário abaixo.</p>
+        </div>
+    );
+};
+export default Contact;
